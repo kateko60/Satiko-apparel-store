1955fab10d11f8de364c0cbde5a74788e3e9a79f
Kateko <nkatekotivane87@gmail.com>
2024-08-12 17:16:37 +0200

    not bug please

---
+++ app.js
@@ -0,0 +1,35 @@
+const slides = document.querySelectorAll(".slides img");
+let slideindex = 0;
+let intervalID = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlides);
+
+function initializeSlides(){
+    if(slides.length > 0){
+        slides[slideindex].classList.add("displaySlider");
+        intervalID = setInterval(nextSlide, 5000);
+    }
+}
+function showSlide(index){
+    if(index >= slides.length){
+        slideindex = 0;
+    }
+    else if(index < 0){
+        slideindex = slides.length - 1;
+
+    }
+        slides.forEach(slide => {
+            slide.classList.remove("displaySlider");
+        });
+    slides[slideindex].classList.add("displaySlider");
+}
+
+function nextSlide(){
+            slideindex++;
+            showSlide(slideindex);
+        }
+   function prevSlide(){
+    clearInterval(intervalID);
+    slideindex--;
+    showSlide(slideindex);
+   }  
